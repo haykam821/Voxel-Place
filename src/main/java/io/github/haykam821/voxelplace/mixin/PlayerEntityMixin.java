4c076c2f6cf4af034265e9d4e8fa61291c628c2b
haykam821 <24855774+haykam821@users.noreply.github.com>
2020-02-09 11:57:59 -0500

    Enforce wait period for attacking entities

--- src/main/java/io/github/haykam821/voxelplace/mixin/PlayerEntityMixin.java
+++ src/main/java/io/github/haykam821/voxelplace/mixin/PlayerEntityMixin.java
@@ -3,17 +3,25 @@ package io.github.haykam821.voxelplace.mixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import io.github.haykam821.voxelplace.NextInteractionEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.GameMode;
 import net.minecraft.world.World;
 
 @Mixin(PlayerEntity.class)
-public abstract class PlayerEntityMixin implements NextInteractionEntity {
+public abstract class PlayerEntityMixin extends LivingEntity implements NextInteractionEntity {
 	long nextInteraction = 0;
+
+	PlayerEntityMixin(EntityType<? extends LivingEntity> type, World world) {
+		super(type, world);
+	}
 
 	public void updateNextInteraction() {
 		this.setNextInteraction(System.currentTimeMillis() + 5000);
@@ -32,5 +40,18 @@ public abstract class PlayerEntityMixin implements NextInteractionEntity {
 		if (canInteract()) {
 			ci.setReturnValue(true);
 		}
+	}
+
+	@Inject(method = "attack", at = @At("HEAD"), cancellable = true)
+	void preventAttacking(Entity entity, CallbackInfo ci) {
+		if (canInteract()) {
+			ci.cancel();
+		}
+	}
+
+	@Override
+	public void onAttacking(Entity entity) {
+		updateNextInteraction();
+		super.onAttacking(entity);
 	}
 }
