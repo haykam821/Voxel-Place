ae5f76fb9147dc7a0f51ffe9d6f086928e7295c1
haykam821 <24855774+haykam821@users.noreply.github.com>
2020-02-09 13:46:31 -0500

    Add sound when cooldown finishes

--- src/main/java/io/github/haykam821/voxelplace/mixin/PlayerEntityMixin.java
+++ src/main/java/io/github/haykam821/voxelplace/mixin/PlayerEntityMixin.java
@@ -1,5 +1,8 @@
 package io.github.haykam821.voxelplace.mixin;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -7,10 +10,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import io.github.haykam821.voxelplace.NextInteractionEntity;
+import net.minecraft.block.enums.Instrument;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.sound.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.GameMode;
 import net.minecraft.world.World;
@@ -18,6 +23,7 @@ import net.minecraft.world.World;
 @Mixin(PlayerEntity.class)
 public abstract class PlayerEntityMixin extends LivingEntity implements NextInteractionEntity {
 	long nextInteraction = 0;
+	Timer timer = new Timer();
 
 	PlayerEntityMixin(EntityType<? extends LivingEntity> type, World world) {
 		super(type, world);
@@ -25,6 +31,18 @@ public abstract class PlayerEntityMixin extends LivingEntity implements NextInte
 
 	public void updateNextInteraction() {
 		this.setNextInteraction(System.currentTimeMillis() + 5000);
+
+		TimerTask task = new TimerTask() {
+			@Override
+			public void run() {
+				try {
+					((PlayerEntity) (Object) PlayerEntityMixin.this).playSound(Instrument.PLING.getSound(), SoundCategory.MASTER, 3.0F, 1.0F);
+				} catch (Exception error) {
+					System.out.println("Failed to play cooldown sound: " + error);
+				}
+			}
+		};
+		timer.schedule(task, 5000);
 	}
 
 	void setNextInteraction(long newNext) {
